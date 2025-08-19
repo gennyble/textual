0b75b997e3dc6623910c6aa3008cb53855c698f0
gennyble <gen@nyble.dev>
2025-08-18 19:16:25 -0500

    rustfmt

--- fastly-backend/src/main.rs
+++ fastly-backend/src/main.rs
@@ -1,12 +1,12 @@
 use std::{net::SocketAddr, sync::Arc};
 
 use axum::{
-    body::{Bytes, Full},
-    extract::Path,
-    http::StatusCode,
-    response::{IntoResponse, Response},
-    routing::get,
-    Extension, Router,
+	body::{Bytes, Full},
+	extract::Path,
+	http::StatusCode,
+	response::{IntoResponse, Response},
+	routing::get,
+	Extension, Router,
 };
 use common::{FontStyle, FontVariant, FontWeight};
 use fontprovider::CachedFont;
@@ -19,74 +19,74 @@ mod fontprovider;
 
 #[tokio::main]
 async fn main() {
-    tracing_subscriber::fmt::init();
+	tracing_subscriber::fmt::init();
 
-    let provider = FontProvider::new("fonts");
+	let provider = FontProvider::new("fonts");
 
-    let app = Router::new()
-        .route("/font/:family/:style/:weight", get(fonts))
-        .route("/ping", get(ping))
-        .layer(Extension(Arc::new(RwLock::new(provider))));
+	let app = Router::new()
+		.route("/font/:family/:style/:weight", get(fonts))
+		.route("/ping", get(ping))
+		.layer(Extension(Arc::new(RwLock::new(provider))));
 
-    let addr = SocketAddr::from(([0, 0, 0, 0], 2561));
-    tracing::debug!("listening on {addr}");
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap()
+	let addr = SocketAddr::from(([0, 0, 0, 0], 2561));
+	tracing::debug!("listening on {addr}");
+	axum::Server::bind(&addr)
+		.serve(app.into_make_service())
+		.await
+		.unwrap()
 }
 
 #[derive(Debug, Deserialize)]
 struct Font {
-    family: String,
-    style: FontStyle,
-    weight: FontWeight,
+	family: String,
+	style: FontStyle,
+	weight: FontWeight,
 }
 
 async fn fonts(
-    provider: Extension<Arc<RwLock<FontProvider>>>,
-    Path(Font {
-        family,
-        style,
-        weight,
-    }): Path<Font>,
+	provider: Extension<Arc<RwLock<FontProvider>>>,
+	Path(Font {
+		family,
+		style,
+		weight,
+	}): Path<Font>,
 ) -> Response {
-    tracing::info!("request for {family} {style} {weight}");
+	tracing::info!("request for {family} {style} {weight}");
 
-    let font = {
-        let res = {
-            provider
-                .read()
-                .await
-                .variant_cached(&family, FontVariant { style, weight })
-        };
+	let font = {
+		let res = {
+			provider
+				.read()
+				.await
+				.variant_cached(&family, FontVariant { style, weight })
+		};
 
-        match res {
-            CachedFont::Available { font } => font,
-            CachedFont::Known => {
-                let mut lock = provider.write().await;
-                match lock.variant(&family, FontVariant { style, weight }) {
-                    Some(font) => font,
-                    None => return (StatusCode::NOT_FOUND, "not found").into_response(),
-                }
-            }
-            CachedFont::Unknown => {
-                tracing::info!("font unknown");
-                return (StatusCode::NOT_FOUND, "not found").into_response();
-            }
-        }
-    };
+		match res {
+			CachedFont::Available { font } => font,
+			CachedFont::Known => {
+				let mut lock = provider.write().await;
+				match lock.variant(&family, FontVariant { style, weight }) {
+					Some(font) => font,
+					None => return (StatusCode::NOT_FOUND, "not found").into_response(),
+				}
+			}
+			CachedFont::Unknown => {
+				tracing::info!("font unknown");
+				return (StatusCode::NOT_FOUND, "not found").into_response();
+			}
+		}
+	};
 
-    Response::builder()
-        .header("content-type", "application/octet-stream")
-        .status(200)
-        .body(Full::new(Bytes::from(font)))
-        .unwrap()
-        .into_response()
+	Response::builder()
+		.header("content-type", "application/octet-stream")
+		.status(200)
+		.body(Full::new(Bytes::from(font)))
+		.unwrap()
+		.into_response()
 }
 
 async fn ping() -> Response {
-    tracing::debug!("pinged!");
+	tracing::debug!("pinged!");
 
-    (StatusCode::OK, "pong!").into_response()
+	(StatusCode::OK, "pong!").into_response()
 }
